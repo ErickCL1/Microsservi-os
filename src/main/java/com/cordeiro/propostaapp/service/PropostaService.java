430fc94138826297a3776144c913f920094f096f
ErickCL1 <erickcordeiro23@gmail.com>
2024-02-16 13:18:28 -0300

    Criacao do repository

--- src/main/java/com/cordeiro/propostaapp/service/PropostaService.java
+++ src/main/java/com/cordeiro/propostaapp/service/PropostaService.java
@@ -2,12 +2,19 @@ package com.cordeiro.propostaapp.service;
 
 import com.cordeiro.propostaapp.dto.PropostaRequestDto;
 import com.cordeiro.propostaapp.dto.PropostaResponseDto;
+import com.cordeiro.propostaapp.entity.Proposta;
+import com.cordeiro.propostaapp.repository.PropostaRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@AllArgsConstructor
 @Service
 public class PropostaService {
 
+    private PropostaRepository propostaRepository;
+
     public PropostaResponseDto criar(PropostaRequestDto requestDto){
+        propostaRepository.save(new Proposta());
         return null;
 
     }
