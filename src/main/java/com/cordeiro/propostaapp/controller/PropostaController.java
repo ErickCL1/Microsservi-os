84a539f32e2c6b5e03dd85ec18f5cf2b701c7775
ErickCL1 <erickcordeiro23@gmail.com>
2024-02-17 23:57:44 -0300

    201 created

--- src/main/java/com/cordeiro/propostaapp/controller/PropostaController.java
+++ src/main/java/com/cordeiro/propostaapp/controller/PropostaController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping("/proposta")
@@ -27,9 +28,20 @@ public class PropostaController {
         como foi passado abaixo.
 
      */
-    @PostMapping
+    @PostMapping  // MÉTODO RETORNANDO 201 CREATED
     public ResponseEntity<PropostaResponseDto> criar(@RequestBody PropostaRequestDto requestDto){
     PropostaResponseDto response =  propostaService.criar(requestDto);
-    return ResponseEntity.ok(response);
+    return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentRequest()
+            .path("/{id}")
+            .buildAndExpand(response.getId())
+            .toUri())
+            .body(response);
     }
+    //   ----------- MÉTODO RETORNANDO 200 OK -------------------
+    /*
+        public ResponseEntity<PropostaREsponseDto> criar (@RequestBody PropostaRequestDto requestDto)
+        PropostaResponseDto response = propostaService.criar(requestDto);
+        return ResponseEntity.ok(response);
+    */
+
 }
